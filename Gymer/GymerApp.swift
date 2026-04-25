4c73db3b7abd5ef5573eec373b9d8d12d4578ad7
Demokan Turan <123177113+Demokann@users.noreply.github.com>
2026-04-25 19:05:15 +0300

    Initial Commit

---
+++ Gymer/GymerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymerApp.swift
+//  Gymer
+//
+//  Created by Demokan Turan on 25.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct GymerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
